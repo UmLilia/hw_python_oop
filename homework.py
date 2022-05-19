41bfea60d5c0f0e4e95935b09f377a3f67bb7eea
Umanets Liliya <umanets.lilia@gmail.com>
2022-05-19 22:48:57 +0300

    pep8

--- homework.py
+++ homework.py
@@ -75,8 +75,8 @@ class Running(Training):
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        calories = ((self.coeff_calorie_1 * self.get_mean_speed() 
-                    - self.coeff_calorie_2) * self.weight / self.M_IN_KM 
+        calories = ((self.coeff_calorie_1 * self.get_mean_speed()
+                    - self.coeff_calorie_2) * self.weight / self.M_IN_KM
                     * self.duration * self.H_IN_MIN)
         return calories
 
@@ -98,9 +98,9 @@ class SportsWalking(Training):
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий"""
-        calories = ((self.coeff_calorie_1 * self.weight + (self.get_mean_speed()
-                    ** 2 // self.height) * self.coeff_calorie_2 * self.weight)
-                    * self.duration * self.H_IN_MIN)
+        calories = ((self.coeff_calorie_1 * self.weight
+                    + (self.get_mean_speed() ** 2 // self.height)
+                    * self.coeff_calorie_2 * self.weight) * self.duration * self.H_IN_MIN)
         return calories
 
 
